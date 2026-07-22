cde6ad9aacb9a2d3f5d19aa450ac77deea4a3109
bellplaster <marketing@bellplaster.com.au>
2026-07-23 09:19:43 +1000

    Refresh streamlined order form assets

--- public/app.js
+++ public/app.js
@@ -166,7 +166,7 @@ function formatAddressDisplay(value) {
 
 function loadDeliveryRefinement() {
   const script = document.createElement("script");
-  script.src = "/delivery-refinement.js?v=20260722-1";
+  script.src = "/delivery-refinement.js?v=20260723-1";
   script.defer = true;
   document.body.append(script);
 }
