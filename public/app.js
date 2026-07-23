4c83fb7fbc06bf0d0790c18f8028e420ff096081
bellplaster <marketing@bellplaster.com.au>
2026-07-23 14:21:48 +1000

    Refresh delivery refinement asset

--- public/app.js
+++ public/app.js
@@ -168,7 +168,7 @@ function formatAddressDisplay(value) {
 
 function loadDeliveryRefinement() {
   const script = document.createElement("script");
-  script.src = "/delivery-refinement.js?v=20260723-1";
+  script.src = "/delivery-refinement.js?v=20260723-2";
   script.defer = true;
   document.body.append(script);
 }
