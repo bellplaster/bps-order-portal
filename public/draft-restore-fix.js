86da669a40271a96945ab217d45c31471e6d89cc
bellplaster <marketing@bellplaster.com.au>
2026-07-28 11:28:49 +1000

    Refresh fresh order defaults asset after workspace fix

--- public/draft-restore-fix.js
+++ public/draft-restore-fix.js
@@ -1,7 +1,7 @@
 (() => {
   if (document.querySelector('script[data-order-defaults="true"]')) return;
   const script = document.createElement("script");
-  script.src = "/order-defaults.js?v=20260728-1";
+  script.src = "/order-defaults.js?v=20260728-2";
   script.defer = true;
   script.dataset.orderDefaults = "true";
   document.body.append(script);
