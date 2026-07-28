7681fb3f1d146c0a45f7dfdfc308c51916801a29
bellplaster <marketing@bellplaster.com.au>
2026-07-28 16:38:10 +1000

    Refresh authoritative order stylesheet

--- public/review-metrics-refinement.js
+++ public/review-metrics-refinement.js
@@ -122,7 +122,7 @@
       link.dataset.orderFinal = "true";
       document.head.append(link);
     }
-    link.href = "/order-final.css?v=20260728-5";
+    link.href = "/order-final.css?v=20260728-6";
   }
 
   function loadScriptOnce(src, marker) {
