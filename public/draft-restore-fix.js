ec0dfbe4b164dbacacb990910db778813797f39d
bellplaster <marketing@bellplaster.com.au>
2026-07-30 15:47:59 +1000

    Load tab duplication controls

--- public/draft-restore-fix.js
+++ public/draft-restore-fix.js
@@ -39,6 +39,14 @@
     document.body.append(script);
   }
 
+  if (!document.querySelector('script[data-tab-duplicate-refinement="true"]')) {
+    const script = document.createElement("script");
+    script.src = "/tab-duplicate-refinement.js?v=20260730-1";
+    script.defer = true;
+    script.dataset.tabDuplicateRefinement = "true";
+    document.body.append(script);
+  }
+
   const syncRequiredDateState = () => {
     const input = document.getElementById("requiredDate");
     if (!(input instanceof HTMLInputElement)) return false;
