1d94259447712ed19ec3cf7eea68af2f77cada78
bellplaster <marketing@bellplaster.com.au>
2026-07-31 11:17:01 +1000

    Load consolidated review total styles

--- public/review-metrics-refinement.js
+++ public/review-metrics-refinement.js
@@ -122,7 +122,7 @@
       link.dataset.orderFinal = "true";
       document.head.append(link);
     }
-    link.href = "/order-final.css?v=20260730-1";
+    link.href = "/order-final.css?v=20260731-2";
   }
 
   function loadScriptOnce(src, marker) {
