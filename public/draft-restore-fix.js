c44302db06d4abd429f0edb5fe6e892cbfcdab2d
bellplaster <marketing@bellplaster.com.au>
2026-08-01 07:12:59 +1000

    Load inline tab rename interaction

--- public/draft-restore-fix.js
+++ public/draft-restore-fix.js
@@ -19,6 +19,7 @@
     ["order-history-access", "/order-history-access.js?v=20260731-1"],
     ["calendar-control", "/calendar-control.js?v=20260731-4"],
     ["portal-identity-ux", "/portal-identity-ux.js?v=20260731-2"],
+    ["inline-tab-rename", "/inline-tab-rename.js?v=20260801-1"],
   ];
 
   controllers.forEach(([marker, src]) => {
