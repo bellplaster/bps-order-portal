9ce733fb687fe50bb35dd04b640db4dc523e037e
bellplaster <marketing@bellplaster.com.au>
2026-08-07 13:50:18 +1000

    Load tabbed tracks controller

--- public/draft-restore-fix.js
+++ public/draft-restore-fix.js
@@ -29,6 +29,7 @@
     ["remove-rondo-finishing-beads", "/remove-rondo-finishing-beads-20260807.js?v=20260807-1"],
     ["rondo-variant-removals", "/rondo-variant-removals-20260807.js?v=20260807-4"],
     ["studs-bmt-row-order", "/studs-bmt-row-order-20260807.js?v=20260807-1"],
+    ["tracks-bmt-tabs", "/tracks-bmt-tabs-20260807.js?v=20260807-1"],
   ];
 
   controllers.forEach(([marker, src]) => {
