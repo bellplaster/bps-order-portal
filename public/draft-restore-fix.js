6d3463e84efce92f0de3891eba6fc22733de7454
bellplaster <marketing@bellplaster.com.au>
2026-08-07 10:17:19 +1000

    Load Rondo finishing beads removal controller

--- public/draft-restore-fix.js
+++ public/draft-restore-fix.js
@@ -26,6 +26,7 @@
     ["board-area-summary", "/board-area-summary.js?v=20260801-3"],
     ["compound-range-update", "/compound-range-update-20260807.js?v=20260807-1"],
     ["partiwall-wall-track-label", "/partiwall-wall-track-label.js?v=20260807-1"],
+    ["remove-rondo-finishing-beads", "/remove-rondo-finishing-beads-20260807.js?v=20260807-1"],
   ];
 
   controllers.forEach(([marker, src]) => {
